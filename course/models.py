a16fe36cc377674bc8362d419b624245c2a840f5
natalya tomova <natalyayurevna19@gmail.com>
2023-12-19 20:52:16 +1000

    Добавлено поле FK модели Lesson и class Meta

--- course/models.py
+++ course/models.py
@@ -7,20 +7,26 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='course/', verbose_name='Картинка', **NULLABLE)
     description = models.TextField(verbose_name='Описание курса ')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
 
-    def __str__(self):
-        return self.name
-
 
 class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lesson', **NULLABLE)
     name = models.CharField(max_length=150, verbose_name='Название урока')
     description = models.TextField(verbose_name='Описание урока')
     preview = models.ImageField(upload_to='course/lesson', verbose_name='Картинка', **NULLABLE)
     video_url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+        ordering = ('course', 'id',)
+
