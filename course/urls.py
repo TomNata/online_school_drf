4013d90c6ffb93a73acb7439887080e349ef21dd
natalya tomova <natalyayurevna19@gmail.com>
2023-12-23 14:41:34 +1000

    Добавлен параметр 'name'

--- course/urls.py
+++ course/urls.py
@@ -11,9 +11,9 @@ router = DefaultRouter()
 router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
-    path('lesson/', LessonListView.as_view()),
-    path('lesson/<int:pk>/', LessonDetailView.as_view()),
-    path('lesson/create/', LessonCreateView.as_view()),
-    path('lesson/update/<int:pk>/', LessonUpdateView.as_view()),
-    path('lesson/delete/<int:pk>/', LessonDeleteView.as_view()),
+    path('lesson/', LessonListView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonDetailView.as_view(), name='lesson-view'),
+    path('lesson/create/', LessonCreateView.as_view(), name='lesson-add'),
+    path('lesson/update/<int:pk>/', LessonUpdateView.as_view(), name='lesson-edit'),
+    path('lesson/delete/<int:pk>/', LessonDeleteView.as_view(), name='lesson-delete'),
 ] + router.urls
