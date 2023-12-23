09208f5f02a2ff4cdb0f7c06d6abd532020b4437
natalya tomova <natalyayurevna19@gmail.com>
2023-12-23 14:37:30 +1000

    Добавлен параметр read_only в поле lessons_list

--- course/serializers.py
+++ course/serializers.py
@@ -17,7 +17,7 @@ class LessonCreateSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lessons_list = LessonSerializer(source='lesson', many=True)
+    lessons_list = LessonSerializer(source='lesson', many=True, read_only=True)
     lessons_quantity = serializers.IntegerField(source='lesson.all.count', read_only=True)
 
     class Meta:
