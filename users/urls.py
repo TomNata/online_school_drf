56d9c6bd76b6cac152949e5a484d28d7db4825db
natalya tomova <natalyayurevna19@gmail.com>
2023-12-23 14:45:45 +1000

    Корректировка url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,16 @@
 from rest_framework import routers
-
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import UserViewSet
+from users.apps import UsersConfig
+
+
+app_name = UsersConfig.name
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'', UserViewSet, basename='users')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
