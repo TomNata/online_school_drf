1e83de4e5ae628061c0b178089d0fa4e0a6a5986
natalya tomova <natalyayurevna19@gmail.com>
2023-12-23 14:32:49 +1000

    Скорректирован path

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
+from payment.apps import PaymentConfig
 from payment.views import PaymentListAPIView
 
+app_name = PaymentConfig.name
 
 urlpatterns = [
-    path('payment/', PaymentListAPIView.as_view()),
+    path('', PaymentListAPIView.as_view(), name='payment'),
 ]
