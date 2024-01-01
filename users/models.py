19c888cddec0dc78aa3a24bc2a6da5ac0eabeede
natalya tomova <natalyayurevna19@gmail.com>
2024-01-01 21:42:15 +1000

    Поле city сделан NULLABLE

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
     phone = models.CharField(max_length=20, verbose_name='телефон', **NULLABLE)
-    city = models.CharField(max_length=50, verbose_name='город')
+    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
     avatar = models.ImageField(upload_to='users/photo', verbose_name='аватар', **NULLABLE)
 
     USERNAME_FIELD = "email"
